b7e8e259f26c7471c42ac5b808d726d5a2ecb16b
Pontax02 <ppontanilla07@gmail.com>
2025-02-19 11:24:58 +0100

    :construction: chore(Estacion): añadir clase estacion(en progreso)

---
+++ src/main/java/domain/estacion/Estacion.java
@@ -0,0 +1,20 @@
+package domain.estacion;
+
+public class Estacion {
+
+    private int id;
+    private String direccion;
+    private int anclajes;
+
+
+    public Estacion(int id, String direccion, int numAnclajes) {
+        this.id = id;
+        this.direccion = direccion;
+        this.anclajes = anclajes;
+    }
+
+    public void  consultarEstacion(){
+        return
+    }
+
+}
