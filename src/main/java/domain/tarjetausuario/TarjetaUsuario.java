7904950518022345310e1714d50c01536d88ff0e
Pontax02 <ppontanilla07@gmail.com>
2025-02-21 13:39:19 +0100

    :construction_worker: build(TarjetaUsuario): crear clase y constructor tarjetausuario

--- src/main/java/domain/tarjetausuario/TarjetaUsuario.java
+++ src/main/java/domain/tarjetausuario/TarjetaUsuario.java
@@ -1,4 +1,13 @@
 package domain.tarjetausuario;
 
 public class TarjetaUsuario {
+
+    private final String id;
+    private boolean activada;
+
+    public TarjetaUsuario(String id, boolean activada) {
+        this.id = id;
+        this.activada = false;
+    }
+
 }
